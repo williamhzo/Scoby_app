1ce998a41c967485709f9c871c32b72c3d615a54
Florian Jomain <florian.jomain@gmail.com>
2020-04-27 19:02:30 +0200

    feat: add forms with BEM class naming

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,8 +6,12 @@ const bcryptSalt = 10;
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+router.get('/create-account', (req, res, next) => {
+  res.render('./auth/createAccount.hbs');
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('./auth/login.hbs');
 });
 
 module.exports = router;
