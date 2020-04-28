c722fd708e3d48ea313b7e84a9cf8de703cbb176
William Hermozo <william.hermozo@protonmail.com>
2020-04-28 12:46:28 +0200

    feat: add functions to map.js

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -54,3 +54,18 @@ map.on('load', function () {
     },
   });
 });
+
+// Center the map on the coordinates of any clicked symbol from the 'symbols' layer.
+// map.on('click', 'symbols', function(e) {
+//   map.flyTo({ center: e.features[0].geometry.coordinates });
+// });
+
+// Change the cursor to a pointer when the it enters a feature in the 'symbols' layer.
+// map.on('mouseenter', 'symbols', function() {
+//   map.getCanvas().style.cursor = 'pointer';
+// });
+
+// Change it back to a pointer when it leaves.
+// map.on('mouseleave', 'symbols', function() {
+//   map.getCanvas().style.cursor = '';
+// }
