184112b4424f25f34c087f73c5328b6b275a8ac2
William Hermozo <william.hermozo@protonmail.com>
2020-05-01 14:26:21 +0200

    fix: fix routing issue preventing homepage from being displayed if user insn't logged-in

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -3,18 +3,32 @@ const router = express.Router();
 const Item = require('../models/Item');
 const Contact = require('../models/Contact');
 
-
+// router.get('/', (req, res, next) => {
+//   Contact.find({
+//       id_user: res.locals.user._id
+//     })
+//     .then((dbresult) => {
+//       res.render('home', {
+//         contact: dbresult
+//       });
+//     })
+//     .catch(next)
+// });
 
 router.get('/', (req, res, next) => {
-  Contact.find({
-      id_user: res.locals.user._id
+  if (req.session.currentUser) {
+    Contact.find({
+      id_user: res.locals.user._id,
     })
-    .then((dbresult) => {
-      res.render('home', {
-        contact: dbresult
-      });
-    })
-    .catch(next)
+      .then((dbresult) => {
+        res.render('home', {
+          contact: dbresult,
+        });
+      })
+      .catch(next);
+  } else {
+    res.render('home');
+  }
 });
 
 // Axios route
