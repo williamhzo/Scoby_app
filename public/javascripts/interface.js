066f060750787ea76dd8bf1a21fe1c61d8a11da9
Florian Jomain <florian.jomain@gmail.com>
2020-05-01 07:15:57 +0200

    style: improvements on add item form, responsiveness, close button, changes in error + about pages

--- public/javascripts/interface.js
+++ public/javascripts/interface.js
@@ -1,9 +1,13 @@
 const addItemButton = document.querySelector('.add-item-button');
-const addItemForm = document.querySelector('.add-item__container');
+const addItemForm = document.querySelector('.add-item__form');
+const closeCross = document.querySelector('.closeCross');
 
 addItemButton.addEventListener('click', () => {
-    // addItemForm.classList.toggle('is-visible');
     addItemForm.style.visibility = "visible";
+});
+
+closeCross.addEventListener('click', () => {
+    addItemForm.style.visibility = "hidden";
 });
 
 document.querySelector('.mapboxgl-canvas').addEventListener('click', () => {
